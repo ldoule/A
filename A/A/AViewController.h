fe66b3595ae4cbdd088ba4e58cfafd33cec60620
lcb <liuchengbin@laoyuegou.com>
2019-05-23 18:35:26 +0800

    3

---
+++ A/A/AViewController.h
@@ -0,0 +1,17 @@
+//
+//  AViewController.h
+//  A
+//
+//  Created by lcb on 2019/5/23.
+//  Copyright © 2019 lcb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ A/A/AViewController.m
@@ -0,0 +1,33 @@
+//
+//  AViewController.m
+//  A
+//
+//  Created by lcb on 2019/5/23.
+//  Copyright © 2019 lcb. All rights reserved.
+//
+
+#import "AViewController.h"
+
+@interface AViewController ()
+
+@end
+
+@implementation AViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor=[UIColor redColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
